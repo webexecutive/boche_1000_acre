54872117a507df013ce544a6964054d3d08927b8
webexecutive <webexecutive@firstkiss.in>
2026-06-29 14:13:18 +0530

    resolved routing error in the .htaccess file

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import fs from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'copy-htaccess',
+      closeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, 'public/.htaccess'),
+          path.resolve(__dirname, 'dist/.htaccess')
+        );
+      }
+    }
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
